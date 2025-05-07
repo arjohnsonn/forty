764222832956efdbc19d5384223f0194d842580f
Ethan Lanting <ethanlanting@gmail.com>
2025-05-06 23:48:04 -0500

    feat: ai-sdk revamp (#1)
    * feat: initial commit
    
    * refactor: padding improvements
    
    * feat: new convo -> conversation flow + improvements
    
    * feat: framer
    
    * feat: implement chat streaming functionality and update dependencies
    
    * refactor: remove GPT API integration and related functions
    
    ---------
    
    Co-authored-by: Aiden Johnson <arjohnsonn12@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
+"use client";
+
+import Conversation from "@/components/Conversation";
 import NewConvo from "@/components/NewConvo";
+import { useState } from "react";
 
 export default function Home() {
+  const [initialQuery, setInitialQuery] = useState("");
+  const [showConversation, setShowConversation] = useState(false);
+
+  // Called by NewConvo with initial greeting and user's first query
+  const handleStartAndNewQuery = async (greeting: string, query: string) => {
+    setInitialQuery(query);
+    setShowConversation(true);
+  };
+
   return (
-    <>
-      <NewConvo />
-    </>
+    <div>
+      {!showConversation ? (
+        <NewConvo onSubmit={handleStartAndNewQuery} />
+      ) : (
+        <Conversation title="New chat" initialQuery={initialQuery} />
+      )}
+    </div>
   );
 }
